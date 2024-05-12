c02432b5f165a01999c8b54f7193a900ba814683
christian-bos-graafschapcollege <c.bos@graafschapcollege.nl>
2024-05-12 14:29:22 +0200

    github actione en playwright tests toevoegd

---
+++ tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+import * as path from 'path';
+
+test('has title', async ({ page }) => {
+  // Bepaal het absolute pad naar het HTML-bestand in de 'app' map
+  const htmlFilePath = path.resolve(__dirname, '..', 'app', 'website.html');
+
+  // Navigeer naar het lokale HTML-bestand
+  await page.goto(`file://${htmlFilePath}`);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Mijn fantastische website');
+});
